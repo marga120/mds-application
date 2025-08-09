602bfa057ec9b33278fbcf43eba1e9ef07b47306
Ariq Muldi <ariqmuldi@gmail.com>
2025-08-08 17:57:00 -0700

    Removed / 10.0 in Overall GPA

--- static/js/applicants.js
+++ static/js/applicants.js
@@ -642,7 +642,6 @@ class ApplicantsManager {
                     max="10.0"
                     step="0.01"
                   />
-                  <span class="text-sm text-gray-500">/ 10.0</span>
                   <button
                     id="saveGpaBtn"
                     class="ml-auto px-3 py-1.5 bg-ubc-blue text-white text-sm rounded-md hover:bg-blue-700 transition-colors font-medium"
